df570dd256ada6a69ac1297afb4bdbc1af966d63
Gouky <Gouky@9347fb73-3252-45a8-a961-318b61db2e2b>
2017-07-01 17:02:50 +0000

    Missing VDPStatusWindow files
    git-svn-id: svn://familyhamel.synology.me/MDStudio@51 9347fb73-3252-45a8-a961-318b61db2e2b

---
+++ src/MDStudio/VDPStatusWindow.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace MDStudio
+{
+    partial class VDPStatusWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listVDPStatus = new System.Windows.Forms.ListView();
+            this.Index = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Info = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listVDPStatus
+            // 
+            this.listVDPStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listVDPStatus.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Index,
+            this.Name,
+            this.Value,
+            this.Info});
+            this.listVDPStatus.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listVDPStatus.Location = new System.Drawing.Point(12, 12);
+            this.listVDPStatus.MultiSelect = false;
+            this.listVDPStatus.Name = "listVDPStatus";
+            this.listVDPStatus.Size = new System.Drawing.Size(744, 209);
+            this.listVDPStatus.TabIndex = 0;
+            this.listVDPStatus.UseCompatibleStateImageBehavior = false;
+            this.listVDPStatus.View = System.Windows.Forms.View.Details;
+            // 
+            // Index
+            // 
+            this.Index.Text = "Index";
+            this.Index.Width = 89;
+            // 
+            // Name
+            // 
+            this.Name.Text = "Name";
+            this.Name.Width = 120;
+            // 
+            // Value
+            // 
+            this.Value.Text = "Value";
+            this.Value.Width = 90;
+            // 
+            // Info
+            // 
+            this.Info.Text = "Info";
+            this.Info.Width = 318;
+            // 
+            // VDPStatusWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 233);
+            this.Controls.Add(this.listVDPStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Text = "VDP Status";
+            this.Load += new System.EventHandler(this.VDPStatusWindow_Load);
+            this.VisibleChanged += new System.EventHandler(this.VDPStatusWindow_VisibleChanged);
+            this.Move += new System.EventHandler(this.VDPStatusWindow_Move);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listVDPStatus;
+        private System.Windows.Forms.ColumnHeader Index;
+        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.ColumnHeader Info;
+        private System.Windows.Forms.ColumnHeader Name;
+    }
+}
---
+++ src/MDStudio/VDPStatusWindow.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MDStudio.Properties;
+using DGenInterface;
+
+namespace MDStudio
+{
+    public partial class VDPStatusWindow : Form
+    {
+        MainForm m_Parent;
+
+        private struct VDPRegisterInfo
+        {
+            public VDPRegisterInfo(int index, string name)
+            {
+                this.baseIndex = index;
+                this.name = name;
+                this.value = 0;
+            }
+
+            public int baseIndex;
+            public int value;
+            public string name;
+        };
+
+        private VDPRegisterInfo[] m_VDPRegisters;
+
+        public VDPStatusWindow(MainForm parent)
+        {
+            InitializeComponent();
+
+            m_Parent = parent;
+            Owner = parent;
+            
+            m_VDPRegisters = new VDPRegisterInfo[23]
+            {
+                new VDPRegisterInfo(0x0, "Mode Register 1" ),
+                new VDPRegisterInfo(0x1, "Mode Register 2" ),
+                new VDPRegisterInfo(0x2, "Plane A Name Table Location" ),
+                new VDPRegisterInfo(0x3, "Window Name Table Location" ),
+                new VDPRegisterInfo(0x4, "Plane B Name Table Location" ),
+                new VDPRegisterInfo(0x5, "Sprite Table Location" ),
+                new VDPRegisterInfo(0x6, "Sprite Pattern Generator Base Address" ),
+                new VDPRegisterInfo(0x7, "Background Colour" ),
+                new VDPRegisterInfo(0xA, "Horizontal Interrupt Counter" ),
+                new VDPRegisterInfo(0xB, "Mode Register 3" ),
+                new VDPRegisterInfo(0xC, "Mode Register 4" ),
+                new VDPRegisterInfo(0xD, "Horizontal Scroll Data Location" ),
+                new VDPRegisterInfo(0xE, "Nametable Pattern Generator Base Address" ),
+                new VDPRegisterInfo(0xF, "Auto-Increment Value" ),
+                new VDPRegisterInfo(0x10, "Plane Size" ),
+                new VDPRegisterInfo(0x11, "Window Plane Horizontal Position" ),
+                new VDPRegisterInfo(0x12, "Window Plane Vertical Position" ),
+                new VDPRegisterInfo(0x13, "DMA Length lo" ),
+                new VDPRegisterInfo(0x14, "DMA Length hi" ),
+                new VDPRegisterInfo(0x15, "DMA Source" ),
+                new VDPRegisterInfo(0x16, "DMA Source" ),
+                new VDPRegisterInfo(0x17, "DMA Source" ),
+                new VDPRegisterInfo(0x18, "DMA Source" ),
+            };
+
+            foreach(VDPRegisterInfo register in m_VDPRegisters)
+            {
+                ListViewItem item = new ListViewItem();
+
+                item.Text = String.Format("0x{0:X}", register.baseIndex.ToString());
+                item.SubItems.Add(register.name);
+                item.SubItems.Add(String.Format("0x{0:X}",register.value.ToString()));
+
+                listVDPStatus.Items.Add(item);
+            }
+        }
+
+        private void VDPStatusWindow_VisibleChanged(object sender, EventArgs e)
+        {
+            m_Parent.UpdateViewVDPStatus(Visible);
+        }
+
+        private void VDPStatusWindow_Load(object sender, EventArgs e)
+        {
+            if (Settings.Default.VDPStatusWindowPosition != null)
+            {
+                this.Location = Settings.Default.VDPStatusWindowPosition;
+            }
+        }
+
+        private void VDPStatusWindow_Move(object sender, EventArgs e)
+        {
+            if (this.Visible)
+            {
+                Settings.Default.VDPStatusWindowPosition = this.Location;
+            }
+        }
+
+        public void UpdateView()
+        {
+            if (DGenThread.GetDGen() != null)
+            {
+                for(int i=0; i<m_VDPRegisters.Length; ++i)
+                {
+                    int value = DGenThread.GetDGen().GetVDPRegisterValue(m_VDPRegisters[i].baseIndex);
+                    
+                    if(value != m_VDPRegisters[i].value)
+                    {
+                        this.listVDPStatus.Items[i].SubItems[2].Text = String.Format("0x{0:X}", value);
+
+                        m_VDPRegisters[i].value = value;
+                    }
+                    else
+                    {
+//                         if(this.listVDPStatus.Items[0].SubItems[1].ForeColor == Color.Red)
+//                         {
+//                             this.listVDPStatus.Items[0].SubItems[1].ForeColor = Color.Black;
+//                         }
+                    }
+                }
+            }
+        }
+    }
+}
