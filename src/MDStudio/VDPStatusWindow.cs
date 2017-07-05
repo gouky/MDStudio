926fd82fed96ff4762f573aff2dd4d2c0354b66d
Gouky <Gouky@9347fb73-3252-45a8-a961-318b61db2e2b>
2017-07-05 06:30:14 +0000

    - More VDPRegister description
    git-svn-id: svn://familyhamel.synology.me/MDStudio@64 9347fb73-3252-45a8-a961-318b61db2e2b

--- src/MDStudio/VDPStatusWindow.cs
+++ src/MDStudio/VDPStatusWindow.cs
@@ -16,16 +16,29 @@ namespace MDStudio
     {
         MainForm m_Parent;
         public delegate void FuncUpdateInfo(int itemIndex);
-        
+
+        private struct VDPRegsiterBitfield
+        {
+            public VDPRegsiterBitfield(int bit, string name)
+            {
+                m_Bit = bit;
+                m_Name = name;
+            }
+
+            public int m_Bit;
+            public string m_Name;
+        }
+
         private struct VDPRegisterInfo
         {
-            public VDPRegisterInfo(int itemIndex, int index, string name, FuncUpdateInfo funcUpdate)
+            public VDPRegisterInfo(int itemIndex, int index, string name, FuncUpdateInfo funcUpdate, VDPRegsiterBitfield[] bitfield)
             {
                 this.itemIndex = itemIndex;
                 this.baseIndex = index;
                 this.name = name;
                 this.value = 0;
                 this.funcUpdate = funcUpdate;
+                this.bitField = bitfield;
             }
 
             public int itemIndex;
@@ -33,9 +46,13 @@ namespace MDStudio
             public int value;
             public string name;
             public FuncUpdateInfo funcUpdate;
+            public VDPRegsiterBitfield[] bitField;
         };
 
         private VDPRegisterInfo[] m_VDPRegisters;
+
+        private VDPRegsiterBitfield[] m_VDPModeRegister3Bitfield;
+        private VDPRegsiterBitfield[] m_VDPModeRegister4Bitfield;
 
         public VDPStatusWindow(MainForm parent)
         {
@@ -43,32 +60,52 @@ namespace MDStudio
 
             m_Parent = parent;
             Owner = parent;
-            
+
+            m_VDPModeRegister3Bitfield = new VDPRegsiterBitfield[4]
+            {
+                new VDPRegsiterBitfield(0, "HS2"),
+                new VDPRegsiterBitfield(1, "HS1"),
+                new VDPRegsiterBitfield(2, "VS"),
+                new VDPRegsiterBitfield(3, "IE2"),
+            };
+
+            m_VDPModeRegister4Bitfield = new VDPRegsiterBitfield[8]
+            {
+                new VDPRegsiterBitfield(0, "RS1"),
+                new VDPRegsiterBitfield(1, "LSM0"),
+                new VDPRegsiterBitfield(2, "LSM1"),
+                new VDPRegsiterBitfield(3, "SHI"),
+                new VDPRegsiterBitfield(4, "SPR"),
+                new VDPRegsiterBitfield(5, "HSY"),
+                new VDPRegsiterBitfield(6, "VSY"),
+                new VDPRegsiterBitfield(7, "RS0")
+            };
+
             m_VDPRegisters = new VDPRegisterInfo[23]
             {
-                new VDPRegisterInfo(0, 0x0, "Mode Register 1", null ),
-                new VDPRegisterInfo(1, 0x1, "Mode Register 2", null ),
-                new VDPRegisterInfo(2, 0x2, "Plane A Name Table Location", UpdatePlaneATableLocation ),
-                new VDPRegisterInfo(3, 0x3, "Window Name Table Location", UpdateWindowTableLocation ),
-                new VDPRegisterInfo(4, 0x4, "Plane B Name Table Location", UpdatePlaneBTableLocation ),
-                new VDPRegisterInfo(5, 0x5, "Sprite Table Location", UpdateSpriteTableLocation),
-                new VDPRegisterInfo(6, 0x6, "Sprite Pattern Generator Base Address", null ),
-                new VDPRegisterInfo(7, 0x7, "Background Colour", null ),
-                new VDPRegisterInfo(8, 0xA, "Horizontal Interrupt Counter", null ),
-                new VDPRegisterInfo(9, 0xB, "Mode Register 3", null ),
-                new VDPRegisterInfo(10, 0xC, "Mode Register 4", null ),
-                new VDPRegisterInfo(11, 0xD, "Horizontal Scroll Data Location", null ),
-                new VDPRegisterInfo(12, 0xE, "Nametable Pattern Generator Base Address", null ),
-                new VDPRegisterInfo(13, 0xF, "Auto-Increment Value", null ),
-                new VDPRegisterInfo(14, 0x10, "Plane Size", null ),
-                new VDPRegisterInfo(15, 0x11, "Window Plane Horizontal Position", null ),
-                new VDPRegisterInfo(16, 0x12, "Window Plane Vertical Position", null ),
-                new VDPRegisterInfo(17, 0x13, "DMA Length lo", null ),
-                new VDPRegisterInfo(18, 0x14, "DMA Length hi", null ),
-                new VDPRegisterInfo(19, 0x15, "DMA Source", null ),
-                new VDPRegisterInfo(20, 0x16, "DMA Source", null ),
-                new VDPRegisterInfo(21, 0x17, "DMA Source", null ),
-                new VDPRegisterInfo(22, 0x18, "DMA Source", null ),
+                new VDPRegisterInfo(0, 0x0, "Mode Register 1", null, null ),
+                new VDPRegisterInfo(1, 0x1, "Mode Register 2", null, null ),
+                new VDPRegisterInfo(2, 0x2, "Plane A Name Table Location", UpdatePlaneATableLocation, null ),
+                new VDPRegisterInfo(3, 0x3, "Window Name Table Location", UpdateWindowTableLocation, null ),
+                new VDPRegisterInfo(4, 0x4, "Plane B Name Table Location", UpdatePlaneBTableLocation, null ),
+                new VDPRegisterInfo(5, 0x5, "Sprite Table Location", UpdateSpriteTableLocation, null),
+                new VDPRegisterInfo(6, 0x6, "Sprite Pattern Generator Base Address", null, null ),
+                new VDPRegisterInfo(7, 0x7, "Background Colour", null, null ),
+                new VDPRegisterInfo(8, 0xA, "Horizontal Interrupt Counter", null, null ),
+                new VDPRegisterInfo(9, 0xB, "Mode Register 3", null, m_VDPModeRegister3Bitfield ),
+                new VDPRegisterInfo(10, 0xC, "Mode Register 4", UpdateModeRegister4, m_VDPModeRegister4Bitfield ),
+                new VDPRegisterInfo(11, 0xD, "Horizontal Scroll Data Location", null, null ),
+                new VDPRegisterInfo(12, 0xE, "Nametable Pattern Generator Base Address", null, null ),
+                new VDPRegisterInfo(13, 0xF, "Auto-Increment Value", null, null ),
+                new VDPRegisterInfo(14, 0x10, "Plane Size", null, null ),
+                new VDPRegisterInfo(15, 0x11, "Window Plane Horizontal Position", null, null ),
+                new VDPRegisterInfo(16, 0x12, "Window Plane Vertical Position", null, null ),
+                new VDPRegisterInfo(17, 0x13, "DMA Length lo", null, null ),
+                new VDPRegisterInfo(18, 0x14, "DMA Length hi", null, null ),
+                new VDPRegisterInfo(19, 0x15, "DMA Source", null, null ),
+                new VDPRegisterInfo(20, 0x16, "DMA Source", null, null ),
+                new VDPRegisterInfo(21, 0x17, "DMA Source", null, null ),
+                new VDPRegisterInfo(22, 0x18, "DMA Source", null, null ),
             };
 
             foreach(VDPRegisterInfo register in m_VDPRegisters)
@@ -167,6 +204,28 @@ namespace MDStudio
             int address = ((m_VDPRegisters[itemIndex].value >> 1) & 0x3F) << 11;
 
             this.listVDPStatus.Items[itemIndex].SubItems[3].Text = String.Format("0x{0:X}", address);
+        }
+
+        private string GetRegisterBitFieldInfo(int value, VDPRegsiterBitfield[] bitfields)
+        {
+            string info = "";
+            bool coma = false;
+            
+            foreach(VDPRegsiterBitfield bitfield in bitfields)
+            {
+                if((value & (1<<bitfield.m_Bit)) != 0)
+                {
+                    info += (coma ? ", " : "") + bitfield.m_Name;
+                    coma = true;
+                }
+            }
+
+            return info;
+        }
+
+        public void UpdateModeRegister4(int itemIndex)
+        {
+            this.listVDPStatus.Items[itemIndex].SubItems[3].Text = GetRegisterBitFieldInfo(m_VDPRegisters[itemIndex].value, m_VDPRegisters[itemIndex].bitField);
         }
     }
 }
